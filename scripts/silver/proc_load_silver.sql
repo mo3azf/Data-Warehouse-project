61f43edd1976b54246d8a73d8178bb1719cbcb17
Moaz Fouad Mostafa <103833585+mo3azf@users.noreply.github.com>
2025-03-27 02:08:36 +0200

    Update proc_load_silver.sql

--- scripts/silver/proc_load_silver.sql
+++ scripts/silver/proc_load_silver.sql
@@ -14,11 +14,11 @@ Parameters:
 	  This stored procedure does not accept any parameters or return any values.
 
 Usage Example:
-    EXEC Silver.load_silver;
+    EXEC selver.load_silver;
 ===============================================================================
 */
 
-CREATE OR ALTER PROCEDURE silver.load_silver AS
+CREATE OR ALTER PROCEDURE selver.load_silver AS
 BEGIN
     DECLARE @start_time DATETIME, @end_time DATETIME, @batch_start_time DATETIME, @batch_end_time DATETIME; 
     BEGIN TRY
@@ -31,12 +31,12 @@ BEGIN
 		PRINT 'Loading CRM Tables';
 		PRINT '------------------------------------------------';
 
-		-- Loading silver.crm_cust_info
+		-- Loading selver.crm_cust_info
         SET @start_time = GETDATE();
-		PRINT '>> Truncating Table: silver.crm_cust_info';
-		TRUNCATE TABLE silver.crm_cust_info;
-		PRINT '>> Inserting Data Into: silver.crm_cust_info';
-		INSERT INTO silver.crm_cust_info (
+		PRINT '>> Truncating Table: selver.crm_cust_info';
+		TRUNCATE TABLE selver.crm_cust_info;
+		PRINT '>> Inserting Data Into: selver.crm_cust_info';
+		INSERT INTO selver.crm_cust_info (
 			cst_id, 
 			cst_key, 
 			cst_firstname, 
@@ -51,8 +51,8 @@ BEGIN
 			TRIM(cst_firstname) AS cst_firstname,
 			TRIM(cst_lastname) AS cst_lastname,
 			CASE 
-				WHEN UPPER(TRIM(cst_marital_status)) = 'S' THEN 'Single'
-				WHEN UPPER(TRIM(cst_marital_status)) = 'M' THEN 'Married'
+				WHEN UPPER(TRIM(cst_material_status)) = 'S' THEN 'Single'
+				WHEN UPPER(TRIM(cst_material_status)) = 'M' THEN 'Married'
 				ELSE 'n/a'
 			END AS cst_marital_status, -- Normalize marital status values to readable format
 			CASE 
@@ -60,11 +60,11 @@ BEGIN
 				WHEN UPPER(TRIM(cst_gndr)) = 'M' THEN 'Male'
 				ELSE 'n/a'
 			END AS cst_gndr, -- Normalize gender values to readable format
-			cst_create_date
+			cst_creat_date
 		FROM (
 			SELECT
 				*,
-				ROW_NUMBER() OVER (PARTITION BY cst_id ORDER BY cst_create_date DESC) AS flag_last
+				ROW_NUMBER() OVER (PARTITION BY cst_id ORDER BY cst_creat_date DESC) AS flag_last
 			FROM bronze.crm_cust_info
 			WHERE cst_id IS NOT NULL
 		) t
@@ -73,12 +73,12 @@ BEGIN
         PRINT '>> Load Duration: ' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS NVARCHAR) + ' seconds';
         PRINT '>> -------------';
 
-		-- Loading silver.crm_prd_info
+		-- Loading selver.crm_prd_info
         SET @start_time = GETDATE();
-		PRINT '>> Truncating Table: silver.crm_prd_info';
-		TRUNCATE TABLE silver.crm_prd_info;
-		PRINT '>> Inserting Data Into: silver.crm_prd_info';
-		INSERT INTO silver.crm_prd_info (
+		PRINT '>> Truncating Table: selver.crm_prd_info';
+		TRUNCATE TABLE selver.crm_prd_info;
+		PRINT '>> Inserting Data Into: selver.crm_prd_info';
+		INSERT INTO selver.crm_prd_info (
 			prd_id,
 			cat_id,
 			prd_key,
@@ -90,8 +90,8 @@ BEGIN
 		)
 		SELECT
 			prd_id,
-			REPLACE(SUBSTRING(prd_key, 1, 5), '-', '_') AS cat_id, -- Extract category ID
-			SUBSTRING(prd_key, 7, LEN(prd_key)) AS prd_key,        -- Extract product key
+			REPLACE(SUBSTRING(pdr_key, 1, 5), '-', '_') AS cat_id, -- Extract category ID
+			SUBSTRING(pdr_key, 7, LEN(pdr_key)) AS prd_key,        -- Extract product key
 			prd_nm,
 			ISNULL(prd_cost, 0) AS prd_cost,
 			CASE 
@@ -101,11 +101,9 @@ BEGIN
 				WHEN UPPER(TRIM(prd_line)) = 'T' THEN 'Touring'
 				ELSE 'n/a'
 			END AS prd_line, -- Map product line codes to descriptive values
-			CAST(prd_start_dt AS DATE) AS prd_start_dt,
-			CAST(
-				LEAD(prd_start_dt) OVER (PARTITION BY prd_key ORDER BY prd_start_dt) - 1 
-				AS DATE
-			) AS prd_end_dt -- Calculate end date as one day before the next start date
+			prd_start_dt,
+		 DATEADD(DAY, -1, LEAD(prd_start_dt) OVER (PARTITION BY pdr_key ORDER BY prd_start_dt)) AS prd_end_dt
+
 		FROM bronze.crm_prd_info;
         SET @end_time = GETDATE();
         PRINT '>> Load Duration: ' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS NVARCHAR) + ' seconds';
@@ -113,10 +111,10 @@ BEGIN
 
         -- Loading crm_sales_details
         SET @start_time = GETDATE();
-		PRINT '>> Truncating Table: silver.crm_sales_details';
-		TRUNCATE TABLE silver.crm_sales_details;
-		PRINT '>> Inserting Data Into: silver.crm_sales_details';
-		INSERT INTO silver.crm_sales_details (
+		PRINT '>> Truncating Table: selver.crm_sales_details';
+		TRUNCATE TABLE selver.crm_sales_details;
+		PRINT '>> Inserting Data Into: selver.crm_sales_details';
+		INSERT INTO selver.crm_sales_details (
 			sls_ord_num,
 			sls_prd_key,
 			sls_cust_id,
@@ -161,10 +159,10 @@ BEGIN
 
         -- Loading erp_cust_az12
         SET @start_time = GETDATE();
-		PRINT '>> Truncating Table: silver.erp_cust_az12';
-		TRUNCATE TABLE silver.erp_cust_az12;
-		PRINT '>> Inserting Data Into: silver.erp_cust_az12';
-		INSERT INTO silver.erp_cust_az12 (
+		PRINT '>> Truncating Table: selver.erp_cust_az12';
+		TRUNCATE TABLE selver.erp_cust_az12;
+		PRINT '>> Inserting Data Into: selver.erp_cust_az12';
+		INSERT INTO selver.erp_cust_az12 (
 			cid,
 			bdate,
 			gen
@@ -194,10 +192,10 @@ BEGIN
 
         -- Loading erp_loc_a101
         SET @start_time = GETDATE();
-		PRINT '>> Truncating Table: silver.erp_loc_a101';
-		TRUNCATE TABLE silver.erp_loc_a101;
-		PRINT '>> Inserting Data Into: silver.erp_loc_a101';
-		INSERT INTO silver.erp_loc_a101 (
+		PRINT '>> Truncating Table: selver.erp_loc_a101';
+		TRUNCATE TABLE selver.erp_loc_a101;
+		PRINT '>> Inserting Data Into: selver.erp_loc_a101';
+		INSERT INTO selver.erp_loc_a101 (
 			cid,
 			cntry
 		)
@@ -216,10 +214,10 @@ BEGIN
 		
 		-- Loading erp_px_cat_g1v2
 		SET @start_time = GETDATE();
-		PRINT '>> Truncating Table: silver.erp_px_cat_g1v2';
-		TRUNCATE TABLE silver.erp_px_cat_g1v2;
-		PRINT '>> Inserting Data Into: silver.erp_px_cat_g1v2';
-		INSERT INTO silver.erp_px_cat_g1v2 (
+		PRINT '>> Truncating Table: selver.erp_px_cat_g1v2';
+		TRUNCATE TABLE selver.erp_px_cat_g1v2;
+		PRINT '>> Inserting Data Into: selver.erp_px_cat_g1v2';
+		INSERT INTO selver.erp_px_cat_g1v2 (
 			id,
 			cat,
 			subcat,
